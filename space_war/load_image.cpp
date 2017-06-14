716086b7be124d6029203edc2b020baef78b4a04
Nickolay Sheyko <nickolay.sheyko@gmail.com>
2017-06-14 18:57:07 +0700

    заработало

--- space_war/load_image.cpp
+++ space_war/load_image.cpp
@@ -10,14 +10,15 @@ SDL_Texture* LoadImage(const std::string &filename)
 	SDL_Surface *loadedImage = nullptr;
 	SDL_Texture *texture = nullptr;
 	std::cout << "string " << filename.c_str() << std::endl;
-	loadedImage = SDL_LoadBMP("earth.bmp");
+	loadedImage = SDL_LoadBMP(filename.c_str());
+
 	if (loadedImage != nullptr) {
 		SDL_RenderClear(OutputSingleton::instance()->ren);
 		texture = SDL_CreateTextureFromSurface(OutputSingleton::instance()->ren, loadedImage);
 		SDL_FreeSurface(loadedImage);
 	}
 	else {
-		//std::cout << SDL_GetError() << std::endl;
+		std::cout << SDL_GetError() << std::endl;
 	}
 	return texture;
 }
